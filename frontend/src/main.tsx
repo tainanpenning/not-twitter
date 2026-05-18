1f1b19f94c8262de55e3e607fa84fc1bc6157555
tainanpenning <tainanhartwigpenning@gmail.com>
2026-05-18 09:47:18 -0300

    updating main and app files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
-import store from "./store/index.tsx";
+import { store } from "./store";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
